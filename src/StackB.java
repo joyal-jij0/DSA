32efa8cb895e06451711707ed2b787b113fb2f84
joyalljijo <joyal2405@gmail.com>
2024-01-03 18:35:12 +0530

    learned graphs for exam tomorrow

--- src/StackB.java
+++ src/StackB.java
@@ -1,37 +1,39 @@
 import java.util.Stack;
 
 public class StackB{
+    public static boolean isValid(String str){  // O(n)
+        Stack<Character> s = new Stack<>();
 
-    public static void stockSpan(int[] stocks, int[] span){
-        Stack<Integer> s = new Stack<>();
-        span[0] = 1;
-        s.push(0);
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            //opening
+            if(ch == '(' || ch == '{' || ch == '[' ){
+                s.push(ch);
+            } else {
+                //closing
+                if(s.isEmpty()){
+                    return false;
+                }
+                if((s.peek() == '(' &&  ch == ')') //()
+                || (s.peek() == '{' && ch == '}') //{}
+                || (s.peek() == '[' && ch == ']')) { //[]
+                    s.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
 
-        for(int i=1; i<stocks.length; i++){
-            int currPrice = stocks[i];
-            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
-                s.pop();
-            }
-            if(s.isEmpty()){
-                span[i] = i+1;
-            }
-            else{
-                int prevHigh = s.peek();
-                span[i] = i - prevHigh;
-            }
-
-            s.push(i);
+        if(s.isEmpty()){
+            return true;
+        } else {
+            return false;
         }
     }
 
     public static void main(String[] args){
-        int[] stocks = {100, 80, 60, 70, 60, 85, 100};
-        int[] span = new int[stocks.length];
-        stockSpan(stocks, span);
+        String str = "({})[]"; //true
+        System.out.print(isValid(str));
 
-        for(int i=0; i<span.length; i++){
-            System.out.println(span[i]+" ");
-
-        }
     }
 }
